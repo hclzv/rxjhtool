bdf57478c82f47f992d405af3a2201323eb52fe1
hclzv <hclzv@sina.com>
2025-11-15 11:35:07 +0800

    1135

--- m.py
+++ m.py
@@ -1,11 +1,7 @@
 from PyQt5 import QtCore,QtGui,QtWidgets,uic
 import sys
-class A(QtWidgets.QMainWindow):
-        def __init__(self):
-            super().__init__()
-            self.resize(680,420)
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    window = A()
+    window = QtWidgets.QMainWindow()
     window.show()
     sys.exit(app.exec_())
