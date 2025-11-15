8eb0242074906502a697febe6705524306ab268e
hclzv <hclzv@sina.com>
2025-11-15 11:44:38 +0800

    1144

--- m.py
+++ m.py
@@ -1,7 +1,20 @@
 from PyQt5 import QtCore,QtGui,QtWidgets,uic
 import sys
+class A(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QHBoxLayout()
+
+        for i in range(10):
+            btn = QtWidgets.QPushButton(f"btn {i}")
+            layout.addWidget(btn)
+            
+        self.setLayout(layout)
+
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    window = QtWidgets.QMainWindow()
+    window = A()
     window.show()
     sys.exit(app.exec_())
